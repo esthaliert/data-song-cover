c805d9a06eb91ab7e9fa2ada1dd114a5ae3c0b05
Esther Winkler <estherwinkler@gmx.net>
2022-05-15 15:06:37 +0200

    Get several data from Songs and Artist

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@ const PLAYER = "https://api.spotify.com/v1/me/player";
 const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
 const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
 const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
+const FEATURES = "https://api.spotify.com/v1/audio-features/{{TrackID}}";
+const ANALYSIS = "https://api.spotify.com/v1/audio-analysis/{{TrackID}}";
+const SONG = "https://api.spotify.com/v1/tracks/{{TrackID}}";
+const ARTIST = "https://api.spotify.com/v1/artists/{{ArtistID}}";
 
 function onPageLoad(){
     client_id = localStorage.getItem("client_id");
@@ -87,6 +91,7 @@ function fetchAccessToken( code ){
 }
 
 function refreshAccessToken(){
+    console.log('refresh');
     refresh_token = localStorage.getItem("refresh_token");
     let body = "grant_type=refresh_token";
     body += "&refresh_token=" + refresh_token;
@@ -101,6 +106,8 @@ function callAuthorizationApi(body){
     xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
     xhr.send(body);
     xhr.onload = handleAuthorizationResponse;
+    console.log(client_id);
+    console.log(client_secret);
 }
 
 function handleAuthorizationResponse(){
@@ -167,16 +174,17 @@ function refreshPlaylists(){
 function handlePlaylistsResponse(){
     if ( this.status == 200 ){
         var data = JSON.parse(this.responseText);
-        var tag = document.createElement("img");
-        console.log(data.items);
+        //console.log(data.items);
         removeAllItems( "playlists" );
         data.items.forEach(item => addPlaylist(item));
         document.getElementById('playlists').value = currentPlaylist;
         var coverList = document.createElement("ul");
-        data.items.forEach(function (cover) {
-            var childElement = '<img alt="Playlist Cover" src="' + cover.images[0].url + '">';
+        data.items.forEach(function (playlist) {
+            var itemID = playlist.id;
+            var childElement = '<div class="cover-container" value="' + itemID + '" ><div class="cover-image" alt="Playlist Cover" style=background-image:url("' + playlist.images[0].url + '");></div></div>';
             $(childElement).appendTo('#playlist-covers');
         });
+        handleCoverSizes();
     }
     else if ( this.status == 401 ){
         refreshAccessToken()
@@ -186,6 +194,217 @@ function handlePlaylistsResponse(){
         alert(this.responseText);
     }
 }
+function handleCoverSizes() {
+    var cover = $('.cover-image');
+    var coverWidth = cover.outerWidth();
+    cover.each(function(index) {
+        $(this).css('height', coverWidth + 'px');
+    });
+}
+
+setTimeout(function() { 
+    $('.cover-container').click(function(){
+        var clickedPlaylist = $(this);
+        var playlistID = $(this).attr('value');
+        $('.cover-container').removeClass('clicked');
+        clickedPlaylist.addClass('clicked');
+        $('.song-item').each(function(i) {
+            $(this).remove();
+        });
+        fetchSongs(playlistID);
+    });
+    $("#track-list").on("click", ".song-item", function(){
+        var clickedSong = $(this);
+        var songID = $(this).attr('id');
+        //var playlistID = $(this).attr('value');
+        $('.song-item').removeClass('clicked');
+        clickedSong.addClass('clicked');
+        fetchSong(songID);
+    });
+
+    $('.playlist-ui > div').click(function(){
+        $('.playlist-ui > div').removeClass('active');
+        var playlistID = $('.cover-container.clicked').attr('value');
+        if ($(this).hasClass('play')) {
+            if ($(this).hasClass('pause')) {
+                $(this).removeClass('pause');
+                playSong(playlistID);
+            } else {
+                $(this).addClass('pause');
+
+            }
+        } else if ($(this).hasClass('shuffle')) {
+            $(this).addClass('active');
+            //playSong();
+        } else if ($(this).hasClass('prev')) {
+
+        } else if ($(this).hasClass('next')) {
+            
+        }
+    });
+}, 1000);
+
+function fetchSongs(plID){
+    let playlist_id = plID;
+    if ( playlist_id.length > 0 ){
+        url = TRACKS.replace("{{PlaylistId}}", playlist_id);
+        console.log(url);
+        callApi( "GET", url, null, handleTracksResponse );
+    }
+}
+
+function fetchSong(soID) {
+    let song_id = soID;
+    if ( song_id.length > 0 ){
+        url = SONG.replace("{{TrackID}}", song_id);
+        callApi( "GET", url, null, handleSongResponse );
+    }
+}
+
+function handleSongResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+        console.log('401');
+    }
+    else {
+        console.log(this.responseText);
+        //alert('Bitte wähle einen Song aus!');
+    }
+}
+
+function addTrack(item, index){
+    var trackID = item.track.id;
+    var artistID = item.track.artists[0].id;
+    fetchArtist(artistID);
+    var track = '<li class="song-item" id="' + trackID + '" value="' + index + '" availability="' + item.track.available_markets.length + '" album="' + item.track.album.name + '" duration="' + item.track.duration_ms + '" popularity="' + item.track.popularity + '"><span class="song-title">' + item.track.name + '</span><br/><span class="song-artist" artist-id="' + artistID + '">' + item.track.artists[0].name  + '</span></li>';
+    $(track).appendTo('#track-list');
+}
+
+function fetchArtist(aID) {
+    let artist_id = aID;
+    if ( artist_id.length > 0 ){
+        url = ARTIST.replace("{{ArtistID}}", artist_id);
+        callApi( "GET", url, null, handleArtistResponse );
+    }
+}
+
+function handleArtistResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        var genre = data.genres;
+        console.log(data);
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+        console.log('401');
+    }
+    else {
+        //console.log(this.responseText);
+        alert('Bitte wähle einen Song aus!');
+    }
+}
+
+function getFeatures(){
+    var trID = $('.song-item.clicked').attr('id');
+    url = FEATURES.replace("{{TrackID}}", trID);
+    callApi( "GET", url, null, handleFeatureResponse );
+    getAnalysis();
+}
+
+function handleFeatureResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        var danceabilityValue = data.danceability;
+        var energyValue = data.energy;
+        var keyValue = data.key;
+        var loudnessValue = data.loudness;
+        var speechinessValue = data.speechiness;
+        var acousticnessValue = data.acousticness;
+        var instrumentalnessValue = data.instrumentalness;
+        var livenessValue = data.liveness;
+        var valenceValue = data.valence;
+        var bpm = data.tempo;
+        $('.danceability').css('background-color', 'rgb(' + (danceabilityValue*50)*10 + ',' + (danceabilityValue*30)*10 + ',' + (danceabilityValue*70)*10 + ')');
+        $('.energy').css('background-color', 'rgb(' + (energyValue*50)*5 + ',' + (energyValue*30)*5 + ',' + (energyValue*20)*5 + ')');
+        $('.key').css('background-color', 'rgb(' + (keyValue*30)*8 + ',' + (keyValue*50)*8 + ',' + (keyValue*10)*8 + ')');
+        $('.loudness').css('background-color', 'rgb(' + (loudnessValue*-10)*3 + ',' + (loudnessValue*-20)*3 + ',' + (loudnessValue*-30)*3 + ')');
+        $('.speechiness').css('background-color', 'rgb(' + (speechinessValue*50)*10 + ',' + (speechinessValue*60)*10 + ',' + (speechinessValue*40)*10 + ')');
+        $('.acousticness').css('background-color', 'rgb(' + (acousticnessValue*70)*7 + ',' + (acousticnessValue*60)*6 + ',' + (speechinessValue*70)*7 + ')');
+        $('.instrumentalness').css('background-color', 'rgb(' + (instrumentalnessValue*40)*8 + ',' + (instrumentalnessValue*80)*8 + ',' + (instrumentalnessValue*50)*8 + ')');
+        $('.liveness').css('background-color', 'rgb(' + (livenessValue*30)*5 + ',' + (livenessValue*20)*5 + ',' + (livenessValue*40)*5 + ')');
+        $('.valence').css('background-color', 'rgb(' + (valenceValue*80)*5 + ',' + (valenceValue*80)*5 + ',' + (valenceValue*80)*5 + ')');
+        var tempoValue = 60/bpm;
+        var visualizerCss = 'songbeat ' + tempoValue + 's linear 0 infinite alternate';
+        //$('#visualizer').css('animation', visualizerCss);
+        function loop(){
+            $('#visualizer')
+            .animate({top: "0" }, (tempoValue*1000), "linear")
+            .animate({top:"50px"}, (tempoValue*1000), "linear", loop);
+        }
+        loop();
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+        console.log('401');
+    }
+    else {
+        //console.log(this.responseText);
+        alert('Bitte wähle einen Song aus!');
+    }
+}
+
+function getAnalysis(){
+    var trID = $('.song-item.clicked').attr('id');
+    url = ANALYSIS.replace("{{TrackID}}", trID);
+    callApi( "GET", url, null, handleAnalysisResponse );
+}
+
+function handleAnalysisResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        var sections = data.sections;
+        var beats = data.beats;
+        var sectionTarget = $('#sections');
+        var trackDuration = data.track.duration;
+        for (var i = 0; i < sections.length; i++) {
+            var sectionDuration = sections[i].duration;
+            var sectionPercentage = sectionDuration / trackDuration * 100;
+            var sectionObject = '<div class="section" style="width:' + sectionPercentage + '%;"></div>';
+            $(sectionObject).appendTo(sectionTarget);
+        }
+    }
+    else if ( this.status == 401 ){
+        refreshAccessToken()
+        console.log('401');
+    }
+    else {
+        //console.log(this.responseText);
+        alert('Bitte wähle einen Song aus!');
+    }
+}
+
+function playSong(plID){
+    let playlist_id = plID;
+    let trackIndex = $('.song-item.clicked').attr('value');
+    let album = document.getElementById("album").value;
+    let body = {};
+    if ( album.length > 0 ){
+        body.context_uri = album;
+    }
+    else {
+        body.context_uri = "spotify:playlist:" + playlist_id;
+        console.log(body.context_uri);
+    }
+     body.offset = {};
+     body.offset.position = trackIndex.length > 0 ? Number(trackIndex) : 0;
+     body.offset.position_ms = 0;
+    callApi( "PUT", PLAY + "?device_id=" + deviceId(), JSON.stringify(body), handleApiResponse );
+}
+
 
 function addPlaylist(item){
     let node = document.createElement("option");
@@ -274,8 +493,9 @@ function fetchTracks(){
 function handleTracksResponse(){
     if ( this.status == 200 ){
         var data = JSON.parse(this.responseText);
-        console.log(data);
+        //console.log(data);
         removeAllItems( "tracks" );
+        //console.log(data.items);
         data.items.forEach( (item, index) => addTrack(item, index));
     }
     else if ( this.status == 401 ){
@@ -287,12 +507,6 @@ function handleTracksResponse(){
     }
 }
 
-function addTrack(item, index){
-    let node = document.createElement("option");
-    node.value = index;
-    node.innerHTML = item.track.name + " (" + item.track.artists[0].name + ")";
-    document.getElementById("tracks").appendChild(node); 
-}
 
 function currentlyPlaying(){
     callApi( "GET", PLAYER + "?market=US", null, handleCurrentlyPlayingResponse );
@@ -301,7 +515,7 @@ function currentlyPlaying(){
 function handleCurrentlyPlayingResponse(){
     if ( this.status == 200 ){
         var data = JSON.parse(this.responseText);
-        console.log(data);
+        //console.log(data);
         if ( data.item != null ){
             document.getElementById("albumImage").src = data.item.album.images[0].url;
             document.getElementById("trackTitle").innerHTML = data.item.name;
